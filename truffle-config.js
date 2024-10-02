92b30ac618d64f49e93bc028586caacd6cedfe82
Sumit Prabhakar <152917281+deltaparticle@users.noreply.github.com>
2024-10-02 17:49:48 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -2,14 +2,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Ganache GUI port
-      network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     
+      port: 7545,            
+      network_id: "*",      
     },
   },
   compilers: {
     solc: {
-      version: "0.8.0",      // Fetch exact version from solc-bin
+      version: "0.8.0",      
     },
   },
 };
